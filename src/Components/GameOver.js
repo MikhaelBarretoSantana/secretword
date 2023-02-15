63d5a6ebc6c999e269ea6ed027c8d7a48b1c20df
StarKhael <mikhaelbarretos@gmail.com>
2023-02-15 11:04:01 -0300

    Project SecretWord

---
+++ src/Components/GameOver.js
@@ -0,0 +1,14 @@
+
+import "./GameOver.css"
+
+const GameOver = ({retry, score}) => {
+    return (
+        <div>
+            <h1 className="gameOver"> Game Over</h1>
+            <h2> Sua pontuação foi: <span> {score}  </span></h2>
+            <button onClick={retry}> Resetar Jogo</button>
+        </div>
+    )
+}
+
+export default GameOver
